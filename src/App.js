a263e26f92ae2564a2ca5d91997e42fca3d4a8b3
rabobahago <rabobahago@gmail.com>
2020-07-14 14:02:00 +0100

    components added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Game from './components/Game';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Game/>
     </div>
   );
 }
